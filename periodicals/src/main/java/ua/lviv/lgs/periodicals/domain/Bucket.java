dcade51d9474f406c7053d53edb11a34953b3317
LogosItAcademy <Tester99037@gmail.com>
2020-08-25 17:01:59 +0300

    added Bucket domain model

--- periodicals/src/main/java/ua/lviv/lgs/periodicals/domain/Bucket.java
+++ periodicals/src/main/java/ua/lviv/lgs/periodicals/domain/Bucket.java
@@ -1,5 +1,108 @@
 package ua.lviv.lgs.periodicals.domain;
 
+import java.util.Date;
+
 public class Bucket {
 
+	private Integer id;
+	private Integer UserId;
+	private Integer productId;
+	private Date purchaseDate;
+	
+	public Bucket() {
+	}
+	
+	public Bucket(Integer userId, Integer productId, Date purchaseDate) {
+		UserId = userId;
+		this.productId = productId;
+		this.purchaseDate = purchaseDate;
+	}
+
+	public Bucket(Integer id, Integer userId, Integer productId, Date purchaseDate) {
+		this.id = id;
+		UserId = userId;
+		this.productId = productId;
+		this.purchaseDate = purchaseDate;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getUserId() {
+		return UserId;
+	}
+
+	public void setUserId(Integer userId) {
+		UserId = userId;
+	}
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((UserId == null) ? 0 : UserId.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((productId == null) ? 0 : productId.hashCode());
+		result = prime * result + ((purchaseDate == null) ? 0 : purchaseDate.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bucket other = (Bucket) obj;
+		if (UserId == null) {
+			if (other.UserId != null)
+				return false;
+		} else if (!UserId.equals(other.UserId))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (productId == null) {
+			if (other.productId != null)
+				return false;
+		} else if (!productId.equals(other.productId))
+			return false;
+		if (purchaseDate == null) {
+			if (other.purchaseDate != null)
+				return false;
+		} else if (!purchaseDate.equals(other.purchaseDate))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Bucket [id=" + id + ", UserId=" + UserId + ", productId=" + productId + ", purchaseDate=" + purchaseDate
+				+ "]";
+	}
 }

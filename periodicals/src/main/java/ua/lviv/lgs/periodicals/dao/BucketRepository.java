b5c16e371227becdcb02afdf7414d79d73b8b240
LogosItAcademy <Tester99037@gmail.com>
2020-08-25 19:03:11 +0300

    +

--- periodicals/src/main/java/ua/lviv/lgs/periodicals/dao/BucketRepository.java
+++ periodicals/src/main/java/ua/lviv/lgs/periodicals/dao/BucketRepository.java
@@ -1,13 +1,10 @@
 package ua.lviv.lgs.periodicals.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ua.lviv.lgs.periodicals.domain.Bucket;
-import ua.lviv.lgs.periodicals.domain.User;
 
 public interface BucketRepository extends JpaRepository<Bucket, Integer>{
 
-	List<User> findByEmail(String email);
+	
 }

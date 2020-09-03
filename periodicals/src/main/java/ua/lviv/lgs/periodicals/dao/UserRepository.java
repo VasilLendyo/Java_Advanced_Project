7aac3fb5c2c5c3110f09644ec05ab58c731959c3
LogosItAcademy <Tester99037@gmail.com>
2020-09-03 09:40:41 +0300

    added user bucket functionality

--- periodicals/src/main/java/ua/lviv/lgs/periodicals/dao/UserRepository.java
+++ periodicals/src/main/java/ua/lviv/lgs/periodicals/dao/UserRepository.java
@@ -10,5 +10,5 @@ import ua.lviv.lgs.periodicals.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 
-	Optional<User> findByEmail(String email);
+	public Optional<User> findByEmail(String email);
 }
